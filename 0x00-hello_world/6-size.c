68c84b22f1315cf5cd21322c51ce358b56028304
arisa-e <emmanuelarisa@outlook.com>
2022-06-02 22:02:17 +0300

    updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,18 +8,18 @@
 
 int main(void)
 {
-        char newChar;
-        int newInt;
-        long newLong;
-        long long newLongLong;
-        float newFloat;
+char newChar;
+int newInt;
+long newLong;
+long long newLongLong;
+float newFloat;
 
-        printf("Size of a char: %ld byte(s)\n", sizeof(newChar));
-        printf("Size of an int: %ld byte(s)\n", sizeof(newInt));
-        printf("Size of a long int: %ld byte(s)\n", sizeof(newLong));
-        printf("Size of a long long int: %ld byte(s)\n", sizeof(newLongLong));
-        printf("Size of a float: %ld byte(s)\n", sizeof(newFloat));
+printf("Size of a char: %ld byte(s)\n", sizeof(newChar));
+printf("Size of an int: %ld byte(s)\n", sizeof(newInt));
+printf("Size of a long int: %ld byte(s)\n", sizeof(newLong));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(newLongLong));
+printf("Size of a float: %ld byte(s)\n", sizeof(newFloat));
 
-        return (0);
+return (0);
 
 }
