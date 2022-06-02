5c17e5f1da8f7bcfa6a226d353517accaa062472
arisa-e <emmanuelarisa@outlook.com>
2022-06-02 23:15:16 +0300

    initial commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ long newLong;
 long long newLongLong;
 float newFloat;
 
-printf("Size of a char: %ld byte(s)\n", sizeof(newChar));
+printf("Size of a char: %d byte(s)\n", sizeof(newChar));
 printf("Size of an int: %d byte(s)\n", sizeof(newInt));
 printf("Size of a long int: %ld byte(s)\n", sizeof(newLong));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(newLongLong));
