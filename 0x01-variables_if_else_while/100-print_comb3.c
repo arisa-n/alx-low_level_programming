77c7df44a21e011391c0be64d313de2d656c433d
arisa-e <emmanuelarisa@outlook.com>
2022-06-03 19:43:22 +0300

    loops

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,19 +6,31 @@
  */
 int main(void)
 {
-int i, j;
-for (i = 0;  i < 10; i++)
-{
-for (j = 0; j < 10; j++)
-{
-if (i != j)
-{
-putchar(i + '0');
-putchar(j + '0');
-putchar(',');
-putchar(' ');
-}
-}
-}
-return (0);
+	int i = '0';
+	int j = '0';
+
+	while (i <= '9')
+	{
+		while (j <= '9')
+		{
+			if (!(i > j || i == j))
+			{
+				putchar(i);
+				putchar(j);
+				if (i == '8' && j == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			j++;
+		}
+		j = '0';
+		i++;
+	}
+	return (0);
 }
