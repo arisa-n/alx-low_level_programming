2a0b9e20fb34ade459ec6104b94a9851379c0aa0
arisa-e <emmanuelarisa@outlook.com>
2022-06-07 15:57:40 +0300

    initial commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,14 +3,12 @@
 /**
 * _isalpha - Shows 1 if the input is c
 * letter Another cases, shows 0
-*
 * @c: The character in ASCII code
-*
 * Return: 1 for letters. Returns 0 otherwise
 */
 int _isalpha(int c)
 {
-if (c >= 'a' || c <= 'z')
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
 return (1);
 }
