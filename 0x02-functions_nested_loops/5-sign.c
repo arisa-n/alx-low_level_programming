9fd8e4002de7f76118b907c6bc129deb9f921e01
arisa-e <emmanuelarisa@outlook.com>
2022-06-07 23:38:52 +0300

    update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,15 +11,15 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-putchar('+');
+_putchar('+');
 }
 else if (n < 0)
 {
-putchar('-');
+_putchar('-');
 }
 else
 {
-putchar('0');
+_putchar('0');
 }
 return (0);
 }
