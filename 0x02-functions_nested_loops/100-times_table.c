fdce1c0734fccdad884201086024f1a78c78c65f
arisa-e <emmanuelarisa@outlook.com>
2022-06-07 21:47:04 +0300

    update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,27 +8,36 @@
 void print_times_table(int n)
 {
 int a, b, c;
-if (n > 15 || n < 0)
+if (n >= 0 && n <= 15)
 {
-printf("%d is not between 0 and 15\n", n);
-return;
-}
 for (a = 0; a <= n; a++)
 {
-for (b = 0; b <= n; b++)
+putchar(48);
+for (b = 1; b <= n; b++)
 {
 c = a * b;
-if (c > 9)
-{
-putchar(c / 10 + '0');
-}
-putchar(c % 10 + '0');
-if (b < n)
-{
 putchar(44);
 putchar(32);
+if (c <= 9)
+{
+putchar(32);
+putchar(32);
+putchar(c + 48);
+}
+else if (c <= 99)
+{
+putchar(32);
+putchar((c / 10) + 48);
+putchar((c % 10) + 48);
+}
+else
+{
+putchar(((c / 100) % 10) + 48);
+putchar(((c / 10) % 10) + 48);
+putchar((c % 10) + 48);
 }
 }
 putchar('\n');
+}
 }
 }
