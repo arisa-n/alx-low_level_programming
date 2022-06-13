3e632013b1254446c5505d3de6456452b4e83c07
arisa-e <emmanuelarisa@outlook.com>
2022-06-13 13:25:01 +0300

    initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * reset_to_98 - function that changes the value of an argument to 98
+ * @n: pointer to the argument
+ * Return: nothing
+ **/
+void reset_to_98(int *n)
+{
+if (n != NULL)
+{
+*n = 98;
+}
+}
