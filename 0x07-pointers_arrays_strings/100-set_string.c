b636d8742858715007d81dc1d9e32073dad53479
arisa-e <emmanuelarisa@outlook.com>
2022-06-20 19:56:07 +0300

    initial commit

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include"main.h"
+/**
+ * set_string - set a string to a value
+ * @s: The string
+ * @to: The value to set the string to
+ */
+void set_string(char **s, char *to)
+{
+    *s = to;
+}
